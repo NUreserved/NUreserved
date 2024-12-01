77efb50f412fe7fe5fff4ded7f2ca26f6677f76e
Icermi Ribano <icermiribano@gmail.com>
2024-12-01 17:47:13 +0800

    Nag add lang ako ng Scaffold

--- app/src/main/java/com/it235/nureserved/LoginScreen.kt
+++ app/src/main/java/com/it235/nureserved/LoginScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -67,174 +68,183 @@ fun LoginScreen(modifier: Modifier = Modifier){
     var password by remember { mutableStateOf("") }
     var passwordVisible by remember { mutableStateOf(false) }
 
-    Box(
-        modifier = modifier
+    Scaffold(
+        modifier = Modifier
             .fillMaxSize()
-    ){
-        //background image
-        Image(
-            modifier = Modifier
-                .fillMaxSize(),
-            painter = painterResource(R.drawable.splash_background),
-            contentDescription = "Logo",
-            contentScale = ContentScale.Crop,
-        )
+    ){ innerPadding ->
 
-        //login
-        Column(
+        Box(
             modifier = Modifier
+                .padding(innerPadding)
                 .fillMaxSize()
-                .graphicsLayer{
-                    renderEffect = BlurEffect(radiusX = 15f, radiusY = 15f)
-                },
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
         ){
-            Card(
+            //background image
+            Image(
                 modifier = Modifier
-                    .fillMaxWidth(0.9f),
-                colors = CardDefaults.cardColors(
-                    containerColor = Color(0xFFFFFFFF)
-                )
+                    .fillMaxSize(),
+                painter = painterResource(R.drawable.splash_background),
+                contentDescription = "Logo",
+                contentScale = ContentScale.Crop,
+            )
+
+            //login
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .graphicsLayer{
+                        renderEffect = BlurEffect(radiusX = 15f, radiusY = 15f)
+                    },
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
             ){
-                Column(
+                Card(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                        .fillMaxWidth(0.9f),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color(0xFFFFFFFF)
+                    )
                 ){
-                    Image(
+                    Column(
                         modifier = Modifier
-                            .width(90.dp)
-                            .padding(top = 40.dp),
-                        painter = painterResource(R.drawable.logo),
-                        contentDescription = "",
-                    )
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    Text(
-                        color = Color(0xFF35408e),
-                        text = "NUreserved",
-                        style = TextStyle(
-                            fontFamily = poppinsSemiBoldItalic,
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 24.sp,
-                            fontStyle = FontStyle.Italic
-                        )
-                    )
-
-                    Spacer(modifier = Modifier.height(40.dp))
-
-                    TextField(
-                        value = email,
-                        onValueChange = { email = it },
-                        label = {
-                            Text (
-                                color = Color(0xFFF8F5F5),
-                                text = "Email",
-                                style = TextStyle(
-                                    fontFamily = poppinsMedium,
-                                    fontSize = 18.sp,
-                                    fontWeight = FontWeight.Medium,
-                                )
-                            )
-                        },
-                        colors = TextFieldDefaults.textFieldColors(
-                            containerColor = Color(0xFFAFAFAF),
-                            focusedTextColor = Color(0xFFF8F5F5),
-                            unfocusedTextColor = Color(0xFFF8F5F5),
-                            cursorColor = Color(0xFFF8F5F5),
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent,
-                        ),
-                        shape = RoundedCornerShape(10.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 20.dp, end = 20.dp)
-                    )
-
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    TextField(
-                        value = password,
-                        onValueChange = { password = it },
-                        label = {
-                            Text (
-                                color = Color(0xFFF8F5F5),
-                                text = "Password",
-                                style = TextStyle(
-                                    fontFamily = poppinsMedium,
-                                    fontSize = 18.sp,
-                                    fontWeight = FontWeight.Medium,
-                                )
-                            )
-                        },
-                        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                        trailingIcon = {
-                            val image: ImageVector = if(!passwordVisible) ImageVector.vectorResource(R.drawable.ic_password_visibility_off) else ImageVector.vectorResource(R.drawable.ic_password_visibility_on)
-                            IconButton(onClick = { passwordVisible = !passwordVisible}){
-                                Icon(imageVector = image, contentDescription = if (passwordVisible) "Hide password" else "Show password",
-                                    tint = Color(0xFFF8F5F5))
-                            }
-                        },
-                        colors = TextFieldDefaults.textFieldColors(
-                            containerColor = Color(0xFFAFAFAF),
-                            focusedTextColor = Color(0xFFF8F5F5),
-                            unfocusedTextColor = Color(0xFFF8F5F5),
-                            cursorColor = Color(0xFFF8F5F5),
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent,
-                        ),
-                        shape = RoundedCornerShape(10.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 20.dp, end = 20.dp)
-                    )
-
-                    Spacer(modifier = Modifier.height(15.dp))
-
-                    Button(
-                        onClick = {},
-                        modifier = Modifier
-                            .padding(start = 20.dp, end = 20.dp)
                             .fillMaxWidth(),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color(0xFF35408E),
-                            contentColor = Color(0xFFF8F5F5)
-                        ),
-                        shape = RoundedCornerShape(10.dp)
+                        horizontalAlignment = Alignment.CenterHorizontally,
                     ){
-                        Text (
-                            text = "Log in",
+                        Image(
+                            modifier = Modifier
+                                .width(90.dp)
+                                .padding(top = 40.dp),
+                            painter = painterResource(R.drawable.logo),
+                            contentDescription = "",
+                        )
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Text(
+                            color = Color(0xFF35408e),
+                            text = "NUreserved",
                             style = TextStyle(
-                                fontFamily = poppinsMedium,
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.Medium,
+                                fontFamily = poppinsSemiBoldItalic,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 24.sp,
+                                fontStyle = FontStyle.Italic
                             )
                         )
+
+                        Spacer(modifier = Modifier.height(40.dp))
+
+                        TextField(
+                            value = email,
+                            onValueChange = { email = it },
+                            label = {
+                                Text (
+                                    color = Color(0xFFF8F5F5),
+                                    text = "Email",
+                                    style = TextStyle(
+                                        fontFamily = poppinsMedium,
+                                        fontSize = 18.sp,
+                                        fontWeight = FontWeight.Medium,
+                                    )
+                                )
+                            },
+                            colors = TextFieldDefaults.textFieldColors(
+                                containerColor = Color(0xFFAFAFAF),
+                                focusedTextColor = Color(0xFFF8F5F5),
+                                unfocusedTextColor = Color(0xFFF8F5F5),
+                                cursorColor = Color(0xFFF8F5F5),
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                            ),
+                            shape = RoundedCornerShape(10.dp),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 20.dp, end = 20.dp)
+                        )
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        TextField(
+                            value = password,
+                            onValueChange = { password = it },
+                            label = {
+                                Text (
+                                    color = Color(0xFFF8F5F5),
+                                    text = "Password",
+                                    style = TextStyle(
+                                        fontFamily = poppinsMedium,
+                                        fontSize = 18.sp,
+                                        fontWeight = FontWeight.Medium,
+                                    )
+                                )
+                            },
+                            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                            trailingIcon = {
+                                val image: ImageVector = if(!passwordVisible) ImageVector.vectorResource(R.drawable.ic_password_visibility_off) else ImageVector.vectorResource(R.drawable.ic_password_visibility_on)
+                                IconButton(onClick = { passwordVisible = !passwordVisible}){
+                                    Icon(imageVector = image, contentDescription = if (passwordVisible) "Hide password" else "Show password",
+                                        tint = Color(0xFFF8F5F5))
+                                }
+                            },
+                            colors = TextFieldDefaults.textFieldColors(
+                                containerColor = Color(0xFFAFAFAF),
+                                focusedTextColor = Color(0xFFF8F5F5),
+                                unfocusedTextColor = Color(0xFFF8F5F5),
+                                cursorColor = Color(0xFFF8F5F5),
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                            ),
+                            shape = RoundedCornerShape(10.dp),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 20.dp, end = 20.dp)
+                        )
+
+                        Spacer(modifier = Modifier.height(15.dp))
+
+                        Button(
+                            onClick = {},
+                            modifier = Modifier
+                                .padding(start = 20.dp, end = 20.dp)
+                                .fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xFF35408E),
+                                contentColor = Color(0xFFF8F5F5)
+                            ),
+                            shape = RoundedCornerShape(10.dp)
+                        ){
+                            Text (
+                                text = "Log in",
+                                style = TextStyle(
+                                    fontFamily = poppinsMedium,
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Medium,
+                                )
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(40.dp))
+
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 20.dp, end = 20.dp, bottom = 40.dp),
+                            text = "Login with your office 365 account to use and benefit from the service" +
+                                    "we offer",
+                            style = TextStyle(
+                                fontFamily = poppinsRegular,
+                                fontWeight = FontWeight.Normal,
+                            ),
+                            textAlign = TextAlign.Center,
+                        )
+
                     }
 
-                    Spacer(modifier = Modifier.height(40.dp))
-
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 20.dp, end = 20.dp, bottom = 40.dp),
-                        text = "Login with your office 365 account to use and benefit from the service" +
-                                "we offer",
-                        style = TextStyle(
-                            fontFamily = poppinsRegular,
-                            fontWeight = FontWeight.Normal,
-                        ),
-                        textAlign = TextAlign.Center,
-                    )
-
                 }
-
             }
-        }
 
+        }
     }
+
+
 }
 
 @Preview(showBackground = true)
