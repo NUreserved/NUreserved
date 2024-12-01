4e8c3d1d94d2badd81d44c7db672293a504c0202
Scoofszlo <scoofszlo@gmail.com>
2024-12-01 15:23:56 +0800

    Add splash screen

--- app/src/main/java/com/it235/nureserved/MainActivity.kt
+++ app/src/main/java/com/it235/nureserved/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
 import com.it235.nureserved.ui.theme.NUreservedTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,20 +28,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen()
+            Main()
         }
     }
 }
 
 @Composable
-private fun MainScreen() {
+private fun Main() {
     NUreservedTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+            SplashScreen(innerPadding)
         }
+    }
+}
+
+@Composable
+fun SplashScreen(innerPadding: PaddingValues) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.splash_background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(240.dp)
+        )
     }
 }
 
@@ -50,5 +78,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 )
 @Composable
 fun MainScreenPreview() {
-    MainScreen()
+    Main()
 }
