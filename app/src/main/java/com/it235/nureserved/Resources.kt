2a6c8a12186ee8976973f931fe85eda56dc526b0
Icermi Ribano <icermiribano@gmail.com>
2024-12-01 18:25:51 +0800

    Added .kt file for different type of resources.

---
+++ app/src/main/java/com/it235/nureserved/Resources.kt
@@ -0,0 +1,20 @@
+package com.it235.nureserved
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+
+class Resources{
+    companion object{
+        val poppinsMedium = FontFamily(
+            Font(R.font.poppins_medium)
+        )
+
+        val poppinsSemiBold = FontFamily(
+            Font(R.font.poppins_semibold)
+        )
+
+        val poppinsNormal = FontFamily(
+            Font(R.font.poppins)
+        )
+    }
+}
