dc243e18f8c89eb3f869455dd6bfbbe33e5350f6
Scoofszlo <scoofszlo@gmail.com>
2024-12-01 20:39:19 +0800

    Add transition between splash page and login page

--- app/src/main/java/com/it235/nureserved/MainActivity.kt
+++ app/src/main/java/com/it235/nureserved/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +17,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -21,7 +27,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
-//import com.it235.nureserved.ui.theme.NUreservedTheme
+import com.it235.nureserved.ui.theme.NUreservedTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,15 +42,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Main() {
-//    NUreservedTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            SplashScreen(innerPadding)
+    NUreservedTheme {
+        val showSplash = remember { mutableStateOf(true) }
+
+        LaunchedEffect(Unit) {
+            delay(1000) // 2000 milliseconds = 2 seconds
+            showSplash.value = false
         }
-//    }
+
+        AnimatedVisibility(
+            visible = showSplash.value,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            SplashScreen()
+        }
+
+        AnimatedVisibility(
+            visible = !showSplash.value,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            LoginScreen()
+        }
+    }
 }
 
 @Composable
-fun SplashScreen(innerPadding: PaddingValues) {
+fun SplashScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
