db9b291c5435943a53211562731f6154a9b363e2
Scoofszlo <scoofszlo@gmail.com>
2024-12-01 20:40:29 +0800

    Remove unused imports and composable

--- app/src/main/java/com/it235/nureserved/MainActivity.kt
+++ app/src/main/java/com/it235/nureserved/MainActivity.kt
@@ -9,13 +9,8 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -88,14 +83,6 @@ fun SplashScreen() {
                 .size(240.dp)
         )
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @PreviewScreenSizes
