0b402801a98884392ef0afc00e688bf08ec7b27c
Josh <joshandre071605@gmail.com>
2024-12-03 20:16:16 +0800

    additional code to AppTermsAndCondition.kt

--- app/src/main/java/com/it235/nureserved/AppTermsAndCondition.kt
+++ app/src/main/java/com/it235/nureserved/AppTermsAndCondition.kt
@@ -1,2 +1,68 @@
 package com.it235.nureserved
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.it235.nureserved.font.poppinsFamily
+
+
+@Composable
+fun RuleComposable(rule: String){
+
+    Spacer(modifier = Modifier.height(10.dp))
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 40.dp, end = 30.dp)
+    ){
+
+        Text(
+            text = "•",
+            style = TextStyle(
+                fontFamily = poppinsFamily,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Normal
+            )
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Text(
+            text = rule,
+            style = TextStyle(
+                fontFamily = poppinsFamily,
+                fontWeight = FontWeight.Normal
+            ),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
