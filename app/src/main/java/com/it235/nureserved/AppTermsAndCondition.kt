2a51288c6aab8fbf75e7f59f1910923844866b92
Josh <joshandre071605@gmail.com>
2024-12-03 21:08:40 +0800

    edits to AppTermsAndCondition.kt

--- app/src/main/java/com/it235/nureserved/AppTermsAndCondition.kt
+++ app/src/main/java/com/it235/nureserved/AppTermsAndCondition.kt
@@ -13,11 +13,20 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,7 +44,7 @@ import com.it235.nureserved.font.poppinsFamily
 
 
 @Composable
-fun RuleComposable(rule: String){
+fun Content(text: String){
 
     Spacer(modifier = Modifier.height(10.dp))
 
@@ -57,7 +66,7 @@ fun RuleComposable(rule: String){
         Spacer(modifier = Modifier.width(10.dp))
 
         Text(
-            text = rule,
+            text = text,
             style = TextStyle(
                 fontFamily = poppinsFamily,
                 fontWeight = FontWeight.Normal
@@ -65,4 +74,66 @@ fun RuleComposable(rule: String){
             textAlign = TextAlign.Justify
         )
     }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Header(
+    title: String,
+    onBackClick: () -> Unit,
+    backgroundColor: Color = Color(0xfff8f5f5)
+){
+
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                fontSize = 20.sp,
+                fontFamily = poppinsFamily,
+                fontWeight = FontWeight.SemiBold,
+                color = Color(0xff333333)
+                )
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back button",
+                    tint = Color(0xff333333)
+                )
+            }
+        },
+
+        colors = topAppBarColors(
+            containerColor = backgroundColor
+        ),
+        modifier = Modifier.fillMaxWidth()
+    )
+
+}
+
+
+
+
+@Composable
+fun AppTermsAndCondition(modifier: Modifier = Modifier){
+
+    Scaffold(){ innerPadding ->
+        Column(
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ){
+            Header(
+                title = "Terms and Conditions",
+                onBackClick = {},
+                backgroundColor = Color(0xfff8f5f5)
+            )
+
+        }
+    }
+
+
 }
