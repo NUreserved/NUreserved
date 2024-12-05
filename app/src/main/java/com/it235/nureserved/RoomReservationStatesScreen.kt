b8eb27af83441a6835a9af5c1fd07291c9ba6330
Icermi Ribano <icermiribano@gmail.com>
2024-12-05 22:47:03 +0800

    Room reservation states screen implementation

---
+++ app/src/main/java/com/it235/nureserved/RoomReservationStatesScreen.kt
@@ -0,0 +1,188 @@
+package com.it235.nureserved
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.it235.nureserved.font.poppinsFamily
+
+@Composable()
+fun StateCard(roomNumber: String, reservationStatus: String, @DrawableRes roomImage: Int, cardContainerColor: Long){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(cardContainerColor),
+            contentColor = Color(0xFFFEFEFE)
+        )
+    ){
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+        ){
+            Image(
+                modifier = Modifier
+                    .weight(2f)
+                    .clip(RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(roomImage),
+                contentDescription = "",
+            )
+
+            Column(
+                modifier = Modifier
+                    .weight(4f)
+                    .fillMaxHeight()
+                    .padding(start = 20.dp),
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = roomNumber,
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium,
+                        fontFamily = poppinsFamily
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(5.dp))
+
+                Text(
+                    text = reservationStatus,
+                    style = TextStyle(
+                        fontWeight = FontWeight.Normal,
+                        fontFamily = poppinsFamily
+                    )
+                )
+            }
+        }
+
+    }
+
+    Spacer(modifier = Modifier.height(10.dp))
+}
+
+//@Composable
+//fun StateCardsContainer(){
+//    Column(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 10.dp)
+//    ){
+//
+//        StateCard(roomNumber = "Room 307", reservationStatus = "Approved: 11:05 am, 11/30/24", roomImage = R.drawable.room202, cardContainerColor = 0xFF49844b)
+//        StateCard(roomNumber = "Room 307", reservationStatus = "Approved: 11:05 am, 11/30/24", roomImage = R.drawable.room202, cardContainerColor = 0xFF49844b)
+//
+//    }
+//}
+
+@Composable
+fun RoomReservationStatesScreen(){
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    val tabs = listOf("Active", "Pending", "History")
+
+    Scaffold(
+
+    ){ innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ){
+            RowHeader(textHeader = "ROOM RESERVATIONS", modifier = Modifier.width(30.dp))
+
+            TabRow(
+                selectedTabIndex = selectedTabIndex,
+                contentColor = Color(0xFF35408E),
+                divider = {},
+                indicator = {tabPositions ->
+                    TabRowDefaults.SecondaryIndicator(
+                        Modifier
+                            .tabIndicatorOffset(tabPositions[selectedTabIndex]),
+                        color = Color(0xFF35408E)
+                    )
+                }
+            ) {
+                tabs.forEachIndexed{ index, title ->
+                    Tab(
+                        selected = selectedTabIndex == index,
+                        onClick = { selectedTabIndex = index },
+                        text = { Text (text = title) }
+                    )
+                }
+            }
+
+            when(selectedTabIndex){
+                0 -> {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 10.dp)
+                    ){
+                        StateCard(roomNumber = "Room 307", reservationStatus = "Approved: 11:05 am, 11/30/24", roomImage = R.drawable.sample_room, cardContainerColor = 0xFF49844b)
+                        StateCard(roomNumber = "Room 307", reservationStatus = "Approved: 11:05 am, 11/30/24", roomImage = R.drawable.sample_room, cardContainerColor = 0xFF49844b)
+                    }
+                }
+                1 -> {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 10.dp)
+                    ){
+                        StateCard(roomNumber = "Room 307", reservationStatus = "Submitted: 11:05 am, 11/30/24", roomImage = R.drawable.sample_room, cardContainerColor = 0xFFd69c40)
+                        StateCard(roomNumber = "Room 307", reservationStatus = "Submitted: 11:05 am, 11/30/24", roomImage = R.drawable.sample_room, cardContainerColor = 0xFFd69c40)
+                    }
+                }
+
+                2 -> {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 10.dp)
+                    ){
+                        StateCard(roomNumber = "Room 307", reservationStatus = "Submitted: 11:05 am, 11/30/24", roomImage = R.drawable.sample_room, cardContainerColor = 0xFFd69c40)
+                        StateCard(roomNumber = "Room 307", reservationStatus = "Submitted: 11:05 am, 11/30/24", roomImage = R.drawable.sample_room, cardContainerColor = 0xFF49844b)
+                        StateCard(roomNumber = "Room 307", reservationStatus = "Submitted: 11:05 am, 11/30/24", roomImage = R.drawable.sample_room, cardContainerColor = 0xFFdb5e5f)
+                    }
+                }
+            }
+
+
+
+        }
+    }
+
+}
