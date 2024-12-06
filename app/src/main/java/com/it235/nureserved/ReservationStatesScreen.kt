8aab0a3cee283c7e328f72077cf44426cefd7968
Icermi Ribano <icermiribano@gmail.com>
2024-12-06 09:14:32 +0800

    Changed from components to composables

--- app/src/main/java/com/it235/nureserved/ReservationStatesScreen.kt
+++ app/src/main/java/com/it235/nureserved/ReservationStatesScreen.kt
@@ -35,7 +35,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.it235.nureserved.components.RowHeader
+import com.it235.nureserved.composables.RowHeader
 import com.it235.nureserved.font.poppinsFamily
 
 @Composable()
