8622ade8e40afb971db795ab50a92c5e01119661
Scoofszlo <scoofszlo@gmail.com>
2024-12-06 17:03:41 +0800

    Add reservation confirmation dialog

--- app/src/main/java/com/it235/nureserved/ui/RoomUsageRulesScreen.kt
+++ app/src/main/java/com/it235/nureserved/ui/RoomUsageRulesScreen.kt
@@ -13,11 +13,14 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -34,6 +38,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.it235.nureserved.R
 import com.it235.nureserved.composables.RowHeader
 import com.it235.nureserved.font.poppinsFamily
 
@@ -71,11 +76,59 @@ fun RuleComposable(rule: String){
 }
 
 @Composable
+fun ReservationConfirmationDialog(
+    showDialog: Boolean,
+    onDismiss: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        title = { Text(text = "Submit request?") },
+        icon = {
+            Icon(
+                painter = painterResource(id = R.drawable.sticky_note_2),
+                contentDescription = "Form icon"
+            )
+       },
+        text = {
+            Text(
+                text = "Are you sure you want to submit the request? This action cannot be undone.",
+                textAlign = TextAlign.Center
+            )
+       },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onDismiss()
+                    // Handle the submit action here
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { onDismiss() }
+            ) {
+                Text("Cancel")
+            }
+        }
+    )
+}
+
+@Composable
 fun RoomUsageRules(navController: NavController){
+    var showDialog by remember { mutableStateOf(false) }
 
     Scaffold(
 
     ){ innerPadding ->
+        if (showDialog) {
+            ReservationConfirmationDialog(
+                showDialog = showDialog,
+                onDismiss = { showDialog = false }
+            )
+        }
+
         Column(
             modifier = Modifier
                 .padding(innerPadding)
@@ -160,7 +213,7 @@ fun RoomUsageRules(navController: NavController){
             ){
 
                 Button(
-                    onClick = {},
+                    onClick = { showDialog = true },
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color(0xFF35408E),
                         contentColor = Color(0xFFFEFEFE)
