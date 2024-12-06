bb57856572f9f18bfd592c496850c63d06e92dc2
Icermi Ribano <icermiribano@gmail.com>
2024-12-06 08:50:54 +0800

    Changed file name

--- app/src/main/java/com/it235/nureserved/RoomUsageRulesScreen.kt
+++ app/src/main/java/com/it235/nureserved/RoomUsageRulesScreen.kt
@@ -1,7 +1,6 @@
 package com.it235.nureserved
 
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
