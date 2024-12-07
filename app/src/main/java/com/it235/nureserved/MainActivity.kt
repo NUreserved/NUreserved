f0a27a7da03b7eff3d4a9bdde3d60d41dfe74d5f
icermi <35890881+icermi@users.noreply.github.com>
2024-12-07 11:08:47 +0800

    Add room reservation form UI (#3)
    * Added .kt file for different type of resources.
    
    * Migrated codes
    
    * Added Scaffold and nested the parent composable representing the room reservation form screen
    
    * Calling room reservation form directly to the entry for testing purposes only
    
    * Deleted Resources.kt, transitioning to other resources
    
    * Using fonts fonts FontFamily.kt
    
    * Added function default parameters
    
    * Applied width to Spacer()
    
    * Applied textHeader to text attribute
    
    * Invoked RowHeader with custom textHeader value
    
    * Changed file name
    
    * Reservation Form Screen logic
    
    * Added file for reusable composables
    
    * Composables logic
    
    * Changed from components to composables
    
    * Redirect the reserve button to `ReservationFormScreen()` screen
    
    * Add room usage rules UI screen (#25)
    
    * Added .kt file for room reservation terms and conditions screen, migrate codes and moving the parent composable representing the screen within the Scaffold
    
    * Temporary calling room reservation terms and condition composable directly to the entry for testing purposes only
    
    * Deleted Resource.kt, transitioning to FontFamily.kt
    
    * Using fonts from FontFamily.kt
    
    * Deleted
    
    * Changed file name
    
    * Room Usage Rules Screen logic
    
    * Changed from components to composables
    
    ---------
    
    Co-authored-by: Icermi Ribano <icermiribano@gmail.com>
    Co-authored-by: icermi <35890881+icermi@users.noreply.github.com>
    
    * Connect the room usage rules screen to room reservation room screen
    
    * Add reservation confirmation dialog
    
    * Add reservation request success dialog; and head back to home screen automatically
    
    * Fix successful dialog showing even if user does not confirm reservation request; fix typos
    
    ---------
    
    Co-authored-by: Scoofszlo <scoofszlo@gmail.com>
    Co-authored-by: Scoofszlo <109350082+Scoofszlo@users.noreply.github.com>

--- app/src/main/java/com/it235/nureserved/MainActivity.kt
+++ app/src/main/java/com/it235/nureserved/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.it235.nureserved.ui.RoomReservationForm
+import com.it235.nureserved.ui.RoomUsageRules
 import com.it235.nureserved.ui.authscreenui.LoginScreen
 import com.it235.nureserved.ui.authscreenui.SignUpScreen
 import com.it235.nureserved.ui.homesreenui.HomeScreen
@@ -62,6 +64,8 @@ private fun Main() {
                 composable(ScreenRoutes.SignUp.route) { SignUpScreen(navController) }
                 composable(ScreenRoutes.Home.route) { HomeScreen(navController) }
                 composable(ScreenRoutes.RoomDetails.route) { RoomDetails(navController)}
+                composable(ScreenRoutes.RoomReservationForm.route){ RoomReservationForm(navController)}
+                composable(ScreenRoutes.RoomUsageRules.route){ RoomUsageRules(navController)}
             }
         }
     }
--- app/src/main/java/com/it235/nureserved/Routes.kt
+++ app/src/main/java/com/it235/nureserved/Routes.kt
@@ -5,4 +5,6 @@ sealed class ScreenRoutes(val route: String) {
     object Home : ScreenRoutes("home")
     object SignUp : ScreenRoutes("signup")
     object RoomDetails : ScreenRoutes("room_details")
+    object RoomReservationForm : ScreenRoutes("room_reservation_form")
+    object RoomUsageRules : ScreenRoutes("room_usage_rules")
 }
