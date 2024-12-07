6c7cc4f8e92ccd359865448ad52465ca8c83c5ed
Icermi Ribano <icermiribano@gmail.com>
2024-12-07 11:52:01 +0800

    Imported Resources

--- app/src/main/java/com/it235/nureserved/ui/reservationscreenui/ReservationStatesScreen.kt
+++ app/src/main/java/com/it235/nureserved/ui/reservationscreenui/ReservationStatesScreen.kt
@@ -2,6 +2,7 @@ package com.it235.nureserved.ui.reservationscreenui
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
