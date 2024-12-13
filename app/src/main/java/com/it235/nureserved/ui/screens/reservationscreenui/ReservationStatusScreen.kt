5fdcd50c7f963b5fd138ce1a3c020782c6bf4c51
Scoofszlo <scoofszlo@gmail.com>
2024-12-13 17:54:19 +0800

    Implement bottom sheet in ReservationStatusScreen

--- app/src/main/java/com/it235/nureserved/ui/screens/reservationscreenui/ReservationStatusScreen.kt
+++ app/src/main/java/com/it235/nureserved/ui/screens/reservationscreenui/ReservationStatusScreen.kt
@@ -19,7 +19,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
@@ -27,11 +29,13 @@ import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -108,6 +112,7 @@ fun StateCard(modifier: Modifier = Modifier, roomNumber: String, reservationStat
     Spacer(modifier = Modifier.height(10.dp))
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RoomReservationStatusScreen(
     navController: NavController,
@@ -116,11 +121,27 @@ fun RoomReservationStatusScreen(
     var selectedTabIndex by remember { mutableStateOf(0) }
     val tabs = listOf("Active", "Pending", "History")
 
+    // Variables for modal bottom sheet implementation
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(innerPadding)
     ){
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState
+            ) {
+                ReservationFilledOutFormScreen(navController)
+            }
+        }
 
         TabRow(
             selectedTabIndex = selectedTabIndex,
@@ -150,7 +171,7 @@ fun RoomReservationStatusScreen(
                         .verticalScroll(rememberScrollState())
                 ) {
                     StateCard(
-                        modifier = Modifier.clickable(onClick = { navController.navigate(ScreenRoutes.ReservationFilledOutForm.route) }),
+                        modifier = Modifier.clickable(onClick = { showBottomSheet = true }),
                         roomNumber = "Room 307",
                         reservationStatus = "Approved: 11:05 am, 11/30/24",
                         roomImage = R.drawable.sample_room,
@@ -158,7 +179,7 @@ fun RoomReservationStatusScreen(
                     )
 
                     StateCard(
-                        modifier = Modifier.clickable(onClick = { navController.navigate(ScreenRoutes.ReservationFilledOutForm.route) }),
+                        modifier = Modifier.clickable(onClick = { showBottomSheet = true }),
                         roomNumber = "Room 307",
                         reservationStatus = "Approved: 11:05 am, 11/30/24",
                         roomImage = R.drawable.sample_room,
