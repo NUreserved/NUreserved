ab3014e6691427be441463f08f016b5935774393
Icermi Ribano <icermiribano@gmail.com>
2024-12-17 14:01:15 +0800

    Remove unnecessary imports

--- app/src/main/java/com/it235/nureserved/ui/screens/authscreenui/LoginScreen.kt
+++ app/src/main/java/com/it235/nureserved/ui/screens/authscreenui/LoginScreen.kt
@@ -6,12 +6,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
