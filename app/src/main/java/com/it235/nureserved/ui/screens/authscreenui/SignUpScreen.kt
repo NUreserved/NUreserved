826dc999671936dcf23ee57c35113e4a6339ac11
Icermi Ribano <icermiribano@gmail.com>
2024-12-24 11:29:00 +0800

    Import resources

--- app/src/main/java/com/it235/nureserved/ui/screens/authscreenui/SignUpScreen.kt
+++ app/src/main/java/com/it235/nureserved/ui/screens/authscreenui/SignUpScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
