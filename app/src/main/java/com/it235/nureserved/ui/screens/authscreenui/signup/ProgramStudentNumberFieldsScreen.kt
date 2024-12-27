3cd7c400b573fba597c3d9d570ebfdaba6a08d27
Icermi Ribano <icermiribano@gmail.com>
2024-12-27 16:15:36 +0800

    Import resources

--- app/src/main/java/com/it235/nureserved/ui/screens/authscreenui/signup/ProgramStudentNumberFieldsScreen.kt
+++ app/src/main/java/com/it235/nureserved/ui/screens/authscreenui/signup/ProgramStudentNumberFieldsScreen.kt
@@ -1,13 +1,16 @@
 package com.it235.nureserved.ui.screens.authscreenui.signup
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -17,6 +20,8 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
