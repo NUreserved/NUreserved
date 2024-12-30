2abe867ccd067ead80bda76128d8ce182a905765
Scoofszlo <scoofszlo@gmail.com>
2024-12-30 18:49:22 +0800

    Revert "Refactor logic"
    This reverts commit 40ede8d571cbd1ddc00ea3a99cb47d58c439deef.

--- app/src/main/java/com/it235/nureserved/ui/screens/authscreenui/signup/ProgramStudentNumberFieldsScreen.kt
+++ app/src/main/java/com/it235/nureserved/ui/screens/authscreenui/signup/ProgramStudentNumberFieldsScreen.kt
@@ -322,7 +322,26 @@ private fun InputField(
         singleLine = true,
         placeholder = { AuthInputPlaceholderTextStyle(label) },
         supportingText = {
-            supportingText()
+            val studentNumberPattern = Regex("^\\d{4}-\\d{6}$")
+
+            if (showSupportText) {
+                if(!studentNumberPattern.containsMatchIn(value)){
+                    isValid.value = false
+                    Text(
+                        text = "Please enter a valid student number format",
+                        color = indicatorColorRed
+                    )
+                }
+
+                else{
+                    isValid.value = true
+                    Text( text = "" )
+                }
+            }
+
+            else{
+                supportingText()
+            }
         },
         colors = TextFieldDefaults.textFieldColors(
             containerColor = white4,
