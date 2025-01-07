a95a306daf856c7db2cda5806915ca65f9a42a3c
Icermi Ribano <icermiribano@gmail.com>
2025-01-07 10:16:44 +0800

    Remove logic

--- app/src/main/java/com/it235/nureserved/composables/SharedComposables.kt
+++ app/src/main/java/com/it235/nureserved/composables/SharedComposables.kt
@@ -112,11 +112,7 @@ fun OnboardingScreen(
     navController: NavController,
     title: String,
     @DrawableRes image: Int,
-    circleFilledIndex: Int,
     description: String,
-    buttonText: String = "",
-    route: List<String>,
-    isTwoButton: Boolean,
 ){
     Scaffold(
         modifier = Modifier
@@ -178,27 +174,6 @@ fun OnboardingScreen(
                 )
             }
 
-            Spacer(modifier = Modifier.height(30.dp))
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-            ){
-                for(i in 0..3){
-                    Image(
-                        painter = if(i == circleFilledIndex) painterResource(id = R.drawable.circle_24dp_35408e_fill1_wght400_grad0_opsz24)
-                        else painterResource(id = R.drawable.circle_24dp_eeeeee_fill1_wght400_grad0_opsz24),
-                        contentDescription = null,
-                    )
-
-                    if(i != 3) Spacer(modifier = Modifier.width(20.dp))
-                }
-
-            }
-
-            Spacer(modifier = Modifier.height(20.dp))
-
             Text(
                 modifier = Modifier
                     .height(80.dp),
@@ -207,68 +182,7 @@ fun OnboardingScreen(
                 textAlign = TextAlign.Center,
             )
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-            ){
-                if(isTwoButton){
-                    OutlinedButton(
-                        onClick = {
-                            navController.navigate(route[0])
-                        },
-                        colors = ButtonDefaults.outlinedButtonColors(
-                            contentColor = if(isSystemInDarkTheme()) white else brandColorBlue
-                        ),
-                        border = BorderStroke(
-                            width = 1.dp,
-                            color = brandColorBlue,
-                        ),
-                    ){
-                        Text( text = "PREV")
-                    }
-
-                    Spacer(modifier = Modifier.width(30.dp))
-
-                    Button(
-                        onClick = {
-                            navController.navigate(route[1])
-                        },
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = brandColorBlue,
-                            contentColor = white3
-                        )
-                    ){
-                        Text( text = "NEXT")
-                    }
-                }
-
-                else{
-                    Button(
-                        onClick = {
-                            if(circleFilledIndex == 3) {
-                                navController.navigate(route[0]){
-                                    popUpTo(0) { inclusive = true }
-                                }
-                            }
-                            else{
-                                navController.navigate(route[0])
-                            }
-                        },
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = brandColorBlue,
-                            contentColor = white3
-                        )
-                    ){
-                        Text(
-                            text = buttonText,
-                            fontSize = 15.sp,
-                        )
-                    }
-                }
-            }
-
-            Spacer(modifier = Modifier.height(90.dp))
+            Spacer(modifier = Modifier.height(140.dp))
 
         }
 
