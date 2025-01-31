52eab91846a0c8942dfba8cd3f6d3ce56aa0842a
Scoofszlo <scoofszlo@gmail.com>
2025-01-31 12:10:16 +0800

    Move `StateCard` to shared composables

--- app/src/main/java/com/it235/nureserved/screens/core/SharedComposables.kt
+++ app/src/main/java/com/it235/nureserved/screens/core/SharedComposables.kt
@@ -9,13 +9,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
@@ -29,7 +33,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -392,4 +398,67 @@ fun RoomReservationFAB(
         },
         onClick = { navController.navigate(ScreenRoutes.RoomReservationForm.route) }
     )
+}
+
+@Composable()
+fun StateCard(
+    modifier: Modifier = Modifier,
+    roomNumber: String,
+    reservationStatus: String,
+    @DrawableRes roomImage: Int,
+    cardContainerColor: Color){
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = cardContainerColor,
+            contentColor = Color(0xFFFEFEFE)
+        )
+    ){
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+        ){
+            Image(
+                modifier = Modifier
+                    .weight(2f)
+                    .clip(RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(roomImage),
+                contentDescription = "",
+            )
+
+            Column(
+                modifier = Modifier
+                    .weight(4f)
+                    .fillMaxHeight()
+                    .padding(start = 15.dp),
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = roomNumber,
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium,
+                        fontFamily = poppinsFamily
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(5.dp))
+
+                Text(
+                    text = reservationStatus,
+                    style = TextStyle(
+                        fontWeight = FontWeight.Normal,
+                        fontFamily = poppinsFamily
+                    )
+                )
+            }
+        }
+
+    }
+
+    Spacer(modifier = Modifier.height(10.dp))
 }
