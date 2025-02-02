17353550870621fe83fde1b29298af3ed1d1dcf6
Scoofszlo <scoofszlo@gmail.com>
2025-02-02 20:15:48 +0800

    Remove unused function

--- app/src/main/java/com/it235/nureserved/screens/admin/reservations/ReservationFormDetailsViewModel.kt
+++ app/src/main/java/com/it235/nureserved/screens/admin/reservations/ReservationFormDetailsViewModel.kt
@@ -61,27 +61,6 @@ class ReservationFormDetailsViewModel : ViewModel() {
         }
     }
 
-    fun getTimeLapseStringForHistory(eventDate: OffsetDateTime): String {
-        val now = OffsetDateTime.now()
-        val duration = Duration.between(eventDate, now)
-
-        val days = duration.toDays()
-        val hours = duration.toHours() % 24
-        val minutes = duration.toMinutes() % 60
-
-        return when {
-            days > 30 -> {
-                val months = days / 30
-                val remainingDays = days % 30
-                "$months m, $remainingDays d"
-            }
-            days > 0 -> "$days d"
-            hours > 0 -> "$hours h"
-            minutes > 1 -> "$minutes m"
-            else -> "Less than a minute ago"
-        }
-    }
-
     fun formatDateFilled(dateTime: OffsetDateTime): String {
         val formattedDateTime = DateTimeFormatter.ofPattern("MMM d, yyyy")
         return dateTime.format(formattedDateTime)
