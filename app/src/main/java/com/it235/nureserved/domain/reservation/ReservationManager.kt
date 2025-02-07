3ba741377eccf31709484565fbe8d06312347a4f
Scoofszlo <scoofszlo@gmail.com>
2025-02-07 18:20:48 +0800

    Create `ReservationManager` for admin

--- app/src/main/java/com/it235/nureserved/domain/reservation/ReservationManager.kt
+++ app/src/main/java/com/it235/nureserved/domain/reservation/ReservationManager.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.it235.nureserved.domain.reservation.ReservationDataMapper.Companion.mapFromModelToDb
+import com.it235.nureserved.domain.reservation.ReservationManager.Companion
+import com.it235.nureserved.domain.reservation.ReservationManager.Companion.retrieveFromDb
 
 class ReservationManager {
     companion object {
@@ -48,6 +50,41 @@ class ReservationManager {
                         callback(reservations)
                     } else {
                         Log.w("ReservationManager", "No reservations found for user")
+                        callback(null)
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Log.w("ReservationManager", "Error getting reservations", e)
+                    callback(null)
+                }
+        }
+    }
+}
+
+class ReservationManagerAdmin {
+    companion object {
+        private val auth = FirebaseAuth.getInstance()
+        private val userId = auth.currentUser?.uid
+
+        fun retrieveReservations(callback: (List<ReservationFormDataV2>) -> Unit) {
+            retrieveFromDb() { data ->
+                callback(ReservationDataMapper.mapFromDbToModel(data))
+            }
+        }
+
+        private fun retrieveFromDb(callback: (List<Map<String, Any>>?) -> Unit) {
+            val db = FirebaseFirestore.getInstance()
+            Log.d("ReservationManager", "Retrieving all reservations")
+
+            db.collection("reservations")
+                .get()
+                .addOnSuccessListener { documents ->
+                    if (!documents.isEmpty) {
+                        val reservations = documents.map { it.data }
+                        Log.d("ReservationManager", "Reservations retrieved: $reservations")
+                        callback(reservations)
+                    } else {
+                        Log.w("ReservationManager", "No reservations found")
                         callback(null)
                     }
                 }
