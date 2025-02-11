c4cc1e2240af2b52ac3dd5d08937518b7e79171e
icermi <35890881+icermi@users.noreply.github.com>
2025-02-11 21:40:25 +0800

    Include fragment-ktx dependency (#196)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore.ktx)
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
