2dccfcce1b8190d22ed8a56d1508b8730770ee41
Scoofszlo <scoofszlo@gmail.com>
2025-02-11 10:36:55 +0800

    Update other UI to comply with new declaration of `TransactionDetails` constant

--- app/src/main/java/com/it235/nureserved/screens/user/reservations/ReservationStatusScreen.kt
+++ app/src/main/java/com/it235/nureserved/screens/user/reservations/ReservationStatusScreen.kt
@@ -257,7 +257,7 @@ private fun ReservationCard(
             containerColor = when (reservation.getLatestTransactionDetails()!!.status) {
                 TransactionStatus.PENDING -> indicatorColorOrange
                 TransactionStatus.APPROVED -> indicatorColorGreen
-                TransactionStatus.DECLINED, TransactionStatus.CANCELLED -> indicatorColorRed
+                TransactionStatus.DECLINED, TransactionStatus.CANCELLED, TransactionStatus.USER_CANCELLED -> indicatorColorRed
             },
             contentColor = Color(0xFFFEFEFE)
         )
