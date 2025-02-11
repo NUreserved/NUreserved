e26f60ea4d81dd209cd9a004ae94872905c378b2
Scoofszlo <scoofszlo@gmail.com>
2025-02-11 18:08:30 +0800

    Show number of pending reservation for each floor on admin side (#192)
    * Import icon
    
    * Add logic for holding value (state) for number of pending reservations
    
    * Add UI logic to show number of pending reservations

--- app/src/main/java/com/it235/nureserved/screens/admin/home/AdminHomeViewModel.kt
+++ app/src/main/java/com/it235/nureserved/screens/admin/home/AdminHomeViewModel.kt
@@ -2,10 +2,27 @@ package com.it235.nureserved.screens.admin.home
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.it235.nureserved.domain.reservation.ReservationFormDataV2
+import com.it235.nureserved.domain.reservation.ReservationManagerAdmin
+import com.it235.nureserved.domain.reservation.TransactionStatus
+import com.it235.nureserved.domain.rooms_v2.FloorLocation
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class AdminHomeViewModel : ViewModel() {
+    private val _reservationList = MutableStateFlow<List<ReservationFormDataV2>>(emptyList())
+
+    private val _secondFloorCount = MutableStateFlow(0)
+    private val _thirdFloorCount = MutableStateFlow(0)
+    private val _fourthFloorCount = MutableStateFlow(0)
+    private val _fifthFloorCount = MutableStateFlow(0)
+
+    private val _floorsCount = MutableStateFlow<List<Int>>(listOf(0, 0, 0, 0))
+    val floorsCount: StateFlow<List<Int>> = _floorsCount
+
     private val _adminHomeScreenIndexStack = MutableStateFlow(listOf<Int>(0))
 
     private val _selectedItem = MutableStateFlow(0)
@@ -19,6 +36,42 @@ class AdminHomeViewModel : ViewModel() {
 
     private val _accountType = "admin"
     val accountType: String = _accountType
+
+    private val _isRefreshing = MutableStateFlow(false)
+    val isRefreshing: StateFlow<Boolean> = _isRefreshing.asStateFlow()
+
+    init {
+        loadReservations()
+    }
+
+    private fun loadReservations(isRefreshing: Boolean = false) {
+        viewModelScope.launch {
+            if (isRefreshing) _isRefreshing.value = true
+
+            ReservationManagerAdmin.retrieveReservations { reservations ->
+                _reservationList.value = reservations
+                Log.d("ReservationsSharedViewModel", "Loaded reservations: ${_reservationList.value.size}")
+
+                countPendingReservationsByFloor()
+
+                if (isRefreshing) _isRefreshing.value = false
+            }
+        }
+    }
+
+    private fun countPendingReservationsByFloor() {
+        val floorCounts = _reservationList.value
+            .filter { it.getLatestTransactionDetails()?.status == TransactionStatus.PENDING }
+            .groupBy { it.getVenue().getOrNull(0)?.location ?: "Unknown" }
+            .mapValues { it.value.size }
+
+        _secondFloorCount.value = floorCounts[FloorLocation.SECOND_FLOOR] ?: 0
+        _thirdFloorCount.value = floorCounts[FloorLocation.THIRD_FLOOR] ?: 0
+        _fourthFloorCount.value = floorCounts[FloorLocation.FOURTH_FLOOR] ?: 0
+        _fifthFloorCount.value = floorCounts[FloorLocation.FIFTH_FLOOR] ?: 0
+
+        _floorsCount.value = listOf(_secondFloorCount.value, _thirdFloorCount.value, _fourthFloorCount.value, _fifthFloorCount.value)
+    }
 
 
     fun onItemSelected(index: Int) {
